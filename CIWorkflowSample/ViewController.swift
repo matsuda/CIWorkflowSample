767c4e0d9af873cc66da2f0cafd6fec85ba81bf6
matsuda <kosukematsuda@gmail.com>
2017-05-02 10:43:59 +0900

    create project

---
+++ CIWorkflowSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CIWorkflowSample
+//
+//  Created by Kosuke Matsuda on 2017/05/02.
+//  Copyright © 2017年 matsuda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
